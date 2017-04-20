d22215874542e603aed16859fca61ec5798655c7
xionx20@sina.cn <killer@killerdeiMac.local>
2017-04-20 17:48:16 +0800

    add target & hook

---
+++ targetapp/app/src/main/java/com/killer/targetapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.killer.targetapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                doThings();
+            }
+        });
+    }
+    public native String doThings();
+    static {
+        System.loadLibrary("targetLib");
+    }
+}
